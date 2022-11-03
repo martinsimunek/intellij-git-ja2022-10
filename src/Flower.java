b256496ebea3a2fc92111db67765881dbf4e5eb8
Martin Šimůnek <public@e-mis.cz>
2022-11-03 20:17:06 +0100

    Flower.toString() added
    To be able to:
    System.out.println(flower);

--- src/Flower.java
+++ src/Flower.java
@@ -12,4 +12,11 @@ public class Flower {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
